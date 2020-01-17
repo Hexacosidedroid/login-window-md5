b34e0acb695a941f6864f7b971778771c5d85c83
Viacheslav Ivanov <tristandtm@gmail.com>
2020-01-17 19:03:52 +0300

    first commit

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Security.Cryptography;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication2
+{
+    
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    public static class DBOperator
+        {
+            private const string CONNECT_STRING = "Data Source=Slava\\1111;Initial Catalog=TDB;Integrated Security=True";
+            public static bool IsUserExists(string name, string password)
+            {
+                string selectCmd = string.Format("SELECT * FROM Qwerty WHERE login='{0}' AND Pass='{1}'",
+                                                 name, password);
+                using (SqlConnection cnn = new SqlConnection(CONNECT_STRING))
+                {
+                    cnn.Open();
+                    using (SqlCommand cmd = new SqlCommand(selectCmd, cnn))
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            return reader.HasRows;
+                        }
+                    }
+                }
+            }
+        }
+        public string GetMD5(string text)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+            byte[] result = md5.Hash;
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                str.Append(result[i].ToString("x2"));
+            }
+            return str.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (DBOperator.IsUserExists(textBox1.Text, GetMD5(textBox2.Text)))
+            {
+                new System.Threading.Thread(() => { Application.Run(new Form2()); }).Start();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль!", "логин неправильный");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ WindowsFormsApplication2/Form2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.polTableAdapter.Fill(this.dBDataSet.pol);
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.polTableAdapter.Update(this.dBDataSet.pol);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (reportViewer1.Visible == false)
+            {
+                reportViewer1.Visible = true;
+            }
+            else
+            {
+                reportViewer1.Visible = false;
+            }
+            }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            int col = polDataGridView.SelectedRows.Count;
+            for (int i = 0; i < col; i++)
+            {
+                polDataGridView.Rows.RemoveAt(polDataGridView.SelectedRows[0].Index);
+            }
+            this.tableAdapterManager.UpdateAll(this.DBDataSet);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            int col = dataGridView1.SelectedRows.Count;
+            for (int i = 0; i < col; i++)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+            }
+            this.tableAdapterManager.UpdateAll(this.DBDataSet);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            int col = dataGridView2.SelectedRows.Count;
+            for (int i = 0; i < col; i++)
+            {
+                dataGridView2.Rows.RemoveAt(dataGridView2.SelectedRows[0].Index);
+            }
+            this.tableAdapterManager.UpdateAll(this.DBDataSet);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            string CONNECT_STRING = "Data Source=Slava\\1111;Initial Catalog=TDB;Integrated Security=True";
+            string c = textBox1.Text, po = textBox2.Text;
+            string selectCmd = string.Format(); //SQl
+            DateTime.Parse(c);
+                DateTime.Parse(po);
+            SqlConnection con = new SqlConnection(CONNECT_STRING);
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = new SqlCommand(selectCmd, con);
+            con.Open();
+            da.Fill(ds, "отчет");
+            dataGridView2.DataSource = ds.Tables[0];
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+        
+    
+}
